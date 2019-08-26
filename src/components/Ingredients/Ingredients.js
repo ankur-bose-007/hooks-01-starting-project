a7f124728031455a3b41174287afe687718910ef
Ankur Bose <Ankur.Bose@cognizant.com>
2019-08-26 18:20:07 +0530

    completed hooks

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,59 +1,72 @@
-import React, { useState, useEffect, useCallback } from 'react';
-
+import React, { useReducer , useEffect, useCallback, useMemo } from 'react';
+import ErrorModal from '../UI/ErrorModal';
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
 import Search from './Search';
-
+import useHttp from '../../hooks/http';
+const ingredientReducer=(currentIngredients,action)=>{
+  switch(action.type){
+    case 'SET':
+      return action.ingredients;
+    case 'ADD':
+      return [...currentIngredients,action.ingredient];
+    case 'DELETE':
+      return currentIngredients.filter(ing=> ing.id!==action.id);
+    default:
+      throw new Error('Should not reach');
+  }
+}
 
 const Ingredients = () => {
-  const [userIngredients, setUserIngredients] = useState([]);
+  const [userIngredients,dispatch]=useReducer(ingredientReducer,[]);
+  const {isLoading,error,data,sendRequest,reqExtra,reqIdentifier,clear}=useHttp();
+  useEffect(()=>{
+    if(!isLoading && !error && reqIdentifier==='REMOVE_INGREDIENT'){
+      dispatch({type:'DELETE',id:reqExtra});
+    }else if(!isLoading && !error && reqIdentifier==='ADD_INGREDIENT'){
+       dispatch({type:'ADD',ingredient:{
+          id: data.name,
+          ...reqExtra
+        }});
+    }
+  },[data,reqExtra,reqIdentifier,isLoading,error]);
 
-  /*useEffect(()=>{
-    console.log("Rendering component",userIngredients);
-  },[userIngredients]);*/
-
-  const addIngredientHandler = ingredient => {
-    fetch('https://react-hooks-update-22d06.firebaseio.com/ingredients.json', {
-      method: 'POST',
-      body: JSON.stringify(ingredient),
-      headers: { 'Content-type': 'application/json' }
-    }).then(response => {
-      return response.json()
-    }).then(responseData => {
-      setUserIngredients(prevIngredients => [
-        ...prevIngredients,
-        {
-          id: responseData.name,
-          ...ingredient
-        }
-      ]);
-    });
-
-  }
+  const addIngredientHandler = useCallback(ingredient => {
+    sendRequest('https://react-hooks-update-22d06.firebaseio.com/ingredients.json'
+    ,'POST'
+    ,JSON.stringify(ingredient),ingredient,
+    'ADD_INGREDIENT');
+  },[sendRequest]);
 
   const filteredIngredientsHandler=useCallback(filteredIngredients=>{
-    setUserIngredients(filteredIngredients);
+    //setUserIngredients(filteredIngredients);
+    dispatch({type:'SET',ingredients:filteredIngredients});
   },[]);
 
-  const removeIngredientHandler = ingId => {
-    fetch(`https://react-hooks-update-22d06.firebaseio.com/ingredients/${ingId}.json`, {
-      method: 'DELETE'
-    }).then(response=>
-    setUserIngredients(prevIngredients => {
-      const updatedIngredients = prevIngredients.filter(ingredient => {
-        return ingredient.id !== ingId;
-      })
-      return [...updatedIngredients];
-    }))
-  }
+  const removeIngredientHandler = useCallback(ingId => {
+    sendRequest(`https://react-hooks-update-22d06.firebaseio.com/ingredients/${ingId}.json`,
+    'DELETE',
+    null,
+    ingId,
+    'REMOVE_INGREDIENT'
+    );
+  },[sendRequest]);
+
+
+  const ingredientList=useMemo(()=>{
+    return (
+      <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler} />
+    );
+  },[userIngredients,removeIngredientHandler]);
 
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredientHandler} />
-
+      {error && <ErrorModal onClose={clear}>{error}</ErrorModal>}
+      <IngredientForm onAddIngredient={addIngredientHandler} loading={isLoading}/>
+      
       <section>
         <Search onLoadIngredients={filteredIngredientsHandler}/>
-        <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler} />
+        {ingredientList}
       </section>
     </div>
   );
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -2,43 +2,48 @@ import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
+import useHttp from '../../hooks/http';
+import ErrorModal from '../UI/ErrorModal';
 
 const Search = React.memo(props => {
   const {onLoadIngredients} = props;
   const [enteredFilter, setEnteredFilter] = useState('');
   const inputRef = useRef();
-
+  const {isLoading,error,data,sendRequest,clear}=useHttp();
   useEffect(() => {
     const timer = setTimeout(() => {
       if (enteredFilter === inputRef.current.value) {
         const query = enteredFilter.length === 0 ? '' : `?orderBy="title"&equalTo="${enteredFilter}"`;
-        fetch('https://react-hooks-update-22d06.firebaseio.com/ingredients.json' + query)
-          .then(response => {
-            return response.json()
-          })
-          .then(responseData => {
-            const loadedIngredients = [];
-            for (const key in responseData) {
-              loadedIngredients.push({
-                id: key,
-                title: responseData[key].title,
-                amount: responseData[key].amount
-              });
-            }
-            onLoadIngredients(loadedIngredients);
-          });
+        sendRequest('https://react-hooks-update-22d06.firebaseio.com/ingredients.json' + query
+        ,'GET');
       }
     }, 500);
     return ()=>{
       clearTimeout(timer);
     };
-  }, [enteredFilter, onLoadIngredients,inputRef]);
+  }, [enteredFilter,inputRef, sendRequest]);
+
+  useEffect(()=>{
+    if(!isLoading&&!error&&data){
+      const loadedIngredients = [];
+            for (const key in data) {
+              loadedIngredients.push({
+                id: key,
+                title: data[key].title,
+                amount: data[key].amount
+              });
+            }
+            onLoadIngredients(loadedIngredients);
+    }
+  },[data,isLoading,error,onLoadIngredients]);
 
   return (
     <section className="search">
+      {error && <ErrorModal onClose={clear}>{error}</ErrorModal>}
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
+          {isLoading && <span>Loading...</span>}
           <input
             ref={inputRef}
             type="text"
